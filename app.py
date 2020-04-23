59b714d6fd147f35bf0c05dcf25531313161f526
boonhui91 <boonhui.91@hotmail.com>
2020-04-23 14:14:55 +0000

    Added testing 'create' route

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import pymongo
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_URI = os.environ.get('MONGO_URI')
+client = pymongo.MongoClient(MONGO_URI)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return render_template('create.template.html')
+
+
+@app.route('/',  methods=['POST'])
+def create():
+    client.project3.user.insert_one({
+        "name": request.form.get("user_name"),
+        "experience": request.form.get("experience")
+    })
+    return "user details added"
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
