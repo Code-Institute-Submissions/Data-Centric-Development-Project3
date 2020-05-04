fb898b7f007045c4acf4c93f0b009d9b5274e9a2
boonhui91 <boonhui.91@hotmail.com>
2020-05-04 17:19:18 +0000

    search for species on latest sightings when clicked

--- static/app.js
+++ static/app.js
@@ -1,3 +1,35 @@
 function goBack() {
   window.history.back()
 }
+
+
+
+// to get species name when click on
+$('.get-species').click(function(){
+
+    // to identify when species column is clicked
+    speciescolumn = $(this).attr("class")
+
+    // if click on species column
+    if (speciescolumn == 'species-onclick get-species'){
+        // set species name
+        species_name = $(this).text() 
+        // parse selected species name into search form input
+        $('#allsearch-form').val(species_name)
+        // submit form
+        $("#search-form").submit()
+
+    }
+
+    // else fetch the species name and submit
+    else{
+    // set species name
+    species_name= $(this).siblings('.species-onclick').text() 
+    // parse selected species name into search form input
+    $('#allsearch-form').val(species_name)
+    // submit form
+    $("#search-form").submit()
+    }
+    
+
+})
